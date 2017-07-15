9c12910f42ab95073dc006d7cc7ed7779173c04b
mawa <mawahujihara@gmail.com>
2017-07-15 14:05:18 +0800

    tsconfig update

--- framework/components/index.ts
+++ framework/components/index.ts
@@ -1,11 +1,7 @@
 import Icon from "./ui/icon/index";
-import Button from "./ui/button/index";
 import TabBarIcon from "./ui/tabBarIcon/index";
-import ui_variables from "./ui/theme/theme";
 
 export {
     Icon,
-    Button,
     TabBarIcon,
-    ui_variables,
 }
