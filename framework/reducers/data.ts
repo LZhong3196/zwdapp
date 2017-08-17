24f72ec9ffffb8b0525dfbb4baaf73fa3b8fd515
mawa <mawahujihara@gmail.com>
2017-08-17 09:54:29 +0800

    Reducers - adding not-null-filter

--- framework/reducers/data.ts
+++ framework/reducers/data.ts
@@ -1,5 +1,4 @@
-import * as Immutable from "immutable";
-
+import { fromJS, Iterable } from "immutable";
 import { State, initialState } from "./../store/index";
 import {
     ACTIONTYPES_DATA_UPDATE,
@@ -11,6 +10,12 @@ import {
     ACTIONTYPES_GOODS_UPDATE,
     ACTIONTYPES_SEARCH_UPDATE
 } from "./../constants";
+
+function fromJSFilterNotNull(jsValue: any): any {
+    let value: any = fromJS(jsValue);
+    return Iterable.isIterable(value) ?
+        value.map((x: any) => x === null ? "" : x) : (value === null ? "" : value);
+}
 
 export type AppReducerHandler = (state: State, action: StoreAction) => State;
 type ReducerConfigs = Dictionary<Array<string>>;
@@ -30,7 +35,7 @@ function createReducer(actionTypes: Array<string>): AppReducerHandler {
         let nextState: State;
         if (actionTypes.indexOf(action.type) !== -1) {
             const resetState: boolean = !action.meta || !action.meta.storeKey;
-            nextState = resetState ? Immutable.fromJS(action.payload) : state.setIn(action.meta.storeKey.split("."), Immutable.fromJS(action.payload));
+            nextState = resetState ? fromJSFilterNotNull(action.payload) : state.setIn(action.meta.storeKey.split("."), fromJSFilterNotNull(action.payload));
         }
         return nextState || state;
     };
@@ -44,6 +49,6 @@ function combineDataReducers(reducerConfigs: ReducerConfigs) {
     return reducers;
 }
 
-const   dataReducers: Dictionary<AppReducerHandler> = combineDataReducers(reducerConfigs);
+const dataReducers: Dictionary<AppReducerHandler> = combineDataReducers(reducerConfigs);
 
 export default dataReducers;
